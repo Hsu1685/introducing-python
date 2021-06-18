b0f39824e703fb3303ce84ac8f7a76fe54fe60f2
Chihmin <chihmin.hsu.a5u@tw.denso.com>
2021-06-18 11:31:22 +0800

    Update dishes.py
    1. Put the content of the original main function in'__main__':
    2. Add content to display Process Number
    3. Add the delay time of the washer to simulate the time required for washing
    4. Add explanation for .task_done()

--- ch15/dishes.py
+++ ch15/dishes.py
@@ -1,20 +1,25 @@
 import multiprocessing as mp
+import os
+import time
 
 def washer(dishes, output):
     for dish in dishes:
-        print('Washing', dish, 'dish')
+        print("Process %s says: " % os.getpid(), 'Washing', dish, 'dish')
+        time.sleep(0.5)
         output.put(dish)
 
 def dryer(input):
     while True:
         dish = input.get()
-        print('Drying', dish, 'dish')
-        input.task_done()
+        print("Process %s says: " % os.getpid(), 'Drying', dish, 'dish')
+        input.task_done() # Indicate that a formerly enqueued task is complete.
 
-dish_queue = mp.JoinableQueue()
-dryer_proc = mp.Process(target=dryer, args=(dish_queue,))
-dryer_proc.daemon = True
-dryer_proc.start()
-dishes = ['salad', 'bread', 'entree', 'dessert']
-washer(dishes, dish_queue)
-dish_queue.join()
+if __name__ == '__main__':
+    print("Process %s says: " % os.getpid(), "I'm the main program")
+    dish_queue = mp.JoinableQueue()
+    dryer_proc = mp.Process(target=dryer, args=(dish_queue,))
+    dryer_proc.daemon = True
+    dryer_proc.start()
+    dishes = ['salad', 'bread', 'entree', 'dessert']
+    washer(dishes, dish_queue)
+    dish_queue.join()
